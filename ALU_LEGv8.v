af95b332d0408794b4e79cd932ec8bbe334c1a19
RagingFlames <mclemoret97@gmail.com>
2018-05-09 10:34:24 -0400

    Removed duplicate 'Mux8to1Nbit' from the ALU

--- ALU_LEGv8.v
+++ ALU_LEGv8.v
@@ -49,27 +49,4 @@ module Adder(A, B, C_in, S, C_out);
 			
 endmodule
 
-module Mux8to1Nbit(F, S, I0, I1, I2, I3, I4, I5, I6, I7);
-			parameter N = 64;
-			
-			input [N-1:0] I0, I1, I2, I3, I4, I5, I6, I7;
-			input [2:0]S; 
-			output [N-1:0]F;
-			
-			
-			assign F = S[2] ? (S[1] ? (S[0] ? I7:I6): (S[0] ? I5:I4)) : (S[1] ? (S[0] ? I3:I2): (S[0] ? I1:I0));
-			
-			
-			endmodule
-			
-			module shifter(A, shift_amount, left, right);
-			
-			input [63:0]A;
-			input [5:0]shift_amount;
-			
-			output [63:0]left, right;
-			assign left = A << shift_amount;
-			assign right = A >> shift_amount;
-			
-			endmodule
 			
