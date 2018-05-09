75dbceca653605c0fb01a751f1d9f41496a2eec1
RagingFlames <mclemoret97@gmail.com>
2018-05-09 11:36:38 -0400

    Updated CLA Testbench

--- CLA_testbench.v
+++ CLA_testbench.v
@@ -1,5 +1,4 @@
 module CLA_testbench (); //written 2/4/18
-		//ALU input/outputs: A, B, FS, CO, F, status
 	
 	reg [63:0] A, B; //2 main inputs for ALU
 	reg Cin;
@@ -7,30 +6,19 @@ module CLA_testbench (); //written 2/4/18
 	wire [63:0] Sum;
 	wire Cout;
 	initial begin
-		//FS <= 5'b0;
-    	//CO <= 1'b0;
-			A <= 64'd0;
+		A <= 64'd0;
 		B <= 64'd0;
 		Cin <= 1'b0;
-	//	A <= {$random, $random};
-	//	B <= {$random, $random};
-	//	Cin <= {$random};
-	#1024 $stop; //then stop once all possibilities have been tested (will happen at #320)
+	#1024 $stop; 
 	end
 
 	always begin
 	#5 A <= A + 1'b1; //every 5 ticks
-	//#5	A <= {32'd0, $random};
-	//#5	B <= {32'd0, $random};
-	//#5	Cin <= {$random};
+	#5	A <= {32'd0, 16'd0, $random};
+	#5	B <= {32'd0, 16'd0, $random};
+	#5	Cin <= {$random};
 	end
-	/*
-	always begin
-		#160 CO <= CO + 1'b1; //after it has finished testing all operations with where CO=0, make CO=1 and redo all calculations
-	end
-	*/
-	CLA_64bit dut (Sum, Cout, A, B, Cin); //instantiating ALU module
-	
 
+	CLA_64bit dut (Sum, Cout, A, B, Cin); //instantiating CLA module
 
 endmodule
